ba6aa99e9db41f55153c02027a46109693ad6085
Yurii Bobuskyi <y.bobuskyi@gmail.com>
2025-03-31 23:19:36 +0300

    add fix for form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const inputs = [...document.querySelectorAll('input')];
+
+inputs.map((item) => {
+  const label = document.createElement('label');
+  const parentItem = item.parentNode;
+
+  parentItem.insertBefore(label, item);
+  label.textContent = item.name;
+  label.className = 'field-label';
+  label.setAttribute('for', item.id);
+
+  item.placeholder = item.name.charAt(0).toUpperCase() + item.name.slice(1);
+});
